5d008935bc24b7610d749f9a2d46845d0bf9b444
清风徐来丶 <603568511@qq.com>
2019-02-13 15:32:00 +0800

    2019.02.13  15:31

---
+++ 05快速排序/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05快速排序
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 11, 2, 4, 55, 1, 9, 8, 6 };
+            QuickSortHelper quickSortHelper = new QuickSortHelper();
+            nums= quickSortHelper.QuickSort(nums);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write(nums[i]+"  ");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 05快速排序/QuickSortHelper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05快速排序
+{
+    //从小到大   快速排序
+    public class QuickSortHelper
+    {
+        //快速排序原理  先在int数组中，作为基准数，
+
+
+        public int[] QuickSort(int[] array)
+        {
+            Sort(array, 0, array.Length - 1);
+            return array;
+        }
+
+        /// <summary>
+        /// 排序方法  
+        /// </summary>
+        /// <param name="array">int数组</param>
+        /// <param name="p"></param>
+        /// <param name="r"></param>
+        public void Sort(int[] array, int p, int r)
+        {
+            int q = 0;
+            if (p < r)
+            {
+                q = Partition(array, p, r);
+                Sort(array, p, q - 1);
+                Sort(array, q + 1, r);
+            }
+        }
+
+        public int Partition(int[] array, int p, int r)
+        {
+            int x = array[r];
+            int j = p - 1;
+
+            for (int i = p; i <= r - 1; i++)
+            {
+                if (array[i] <= x)
+                {
+                    j++;
+                    Swap(array, j, i);
+                }
+            }
+
+            Swap(array, j + 1, r);
+            return j + 1;
+        }
+
+
+        /// <summary>
+        /// 交换数组中的指定元素
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        public void Swap(int[] array, int i, int j)
+        {
+            int t = array[i];
+            array[i] = array[j];
+            array[j] = t;
+        }
+
+    }
+}
